29dd787e0e83c517236c0c61f1843b52c667f7cd
Alina <scheglevatyh_aa@nlmk.com>
2022-09-24 16:00:57 +0300

    lab 2

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,8 @@
-import React, {useEffect, useRef, useState} from 'react'
+import React, {useEffect} from 'react'
 let cache = ""
 let cache2 = JSON.parse(localStorage.getItem("cache2") || '[]') || []
 export const Header = ()=> {
-    let search: HTMLInputElement, list: any, removeButton: any
-    const rootEl = useRef(null);
+    let search: HTMLInputElement, list: any
     useEffect(()=>{
         search = document.getElementById('search_input') as HTMLInputElement
         list = document.getElementById('search_list')
@@ -47,12 +46,30 @@ export const Header = ()=> {
     const handleSearch = () =>{
         cache && cache2.push(cache)
         localStorage.setItem('cache2', JSON.stringify(cache2))
-
-        if (list !== null) {
-            list.style.display = 'flex'
-            list.innerHTML += `<div id="search_list-item">${cache}<div id='remove_button'><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M6.062 15 5 13.938 8.938 10 5 6.062 6.062 5 10 8.938 13.938 5 15 6.062 11.062 10 15 13.938 13.938 15 10 11.062Z"/></svg></div></div>`
-        }
+        drawList()
         list.style.display = 'none'
+    }
+
+    // @ts-ignore
+    const drawList = () =>{
+        return (
+            <>
+                {
+                    // @ts-ignore
+                    cache2.map(item => (
+                        <div id="search_list-item" onClick={(e)=>handlerInserting(e)}>
+                            {item}
+                            <div id='remove_button' onClick={(e)=>handlerRemove(e)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20">
+                                    <path
+                                        d="M6.062 15 5 13.938 8.938 10 5 6.062 6.062 5 10 8.938 13.938 5 15 6.062 11.062 10 15 13.938 13.938 15 10 11.062Z"/>
+                                </svg>
+                            </div>
+                        </div>
+                    ))
+                }
+            </>
+        )
     }
 
 
@@ -63,21 +80,10 @@ export const Header = ()=> {
                 <div className="wrapper site-header__wrapper">
                     <a href="#" className="brand">JV Brand</a>
                     <div className='search' id='#search'>
-                        <input ref={rootEl} autoComplete="off" type="text" placeholder="Искать здесь..." id='search_input' onFocus={onFocusHandler} onChange={(event)=>handlerChange(event)}/>
+                        <input autoComplete="off" type="text" placeholder="Искать здесь..." id='search_input' onFocus={onFocusHandler} onChange={(event)=>handlerChange(event)}/>
                         <div id='search_list'>
                             {
-                                // @ts-ignore
-                                cache2.map(item => (
-                                <div id="search_list-item" onClick={(e)=>handlerInserting(e)}>
-                                    {item}
-                                     <div id='remove_button' onClick={(e)=>handlerRemove(e)}>
-                                         <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20">
-                                            <path
-                                                d="M6.062 15 5 13.938 8.938 10 5 6.062 6.062 5 10 8.938 13.938 5 15 6.062 11.062 10 15 13.938 13.938 15 10 11.062Z"/>
-                                        </svg>
-                                    </div>
-                                </div>
-                            ))
+                                drawList()
                             }
                         </div>
                         <button type="submit" id='search_button' onClick={handleSearch}><svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 0 32 32" width="32px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg></button>
